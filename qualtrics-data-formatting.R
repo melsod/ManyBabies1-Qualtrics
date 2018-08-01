0e5e597fd71fd35a298fb2cbb591bd8f938f7dfe
Melanie Soderstrom <melsod@babylanguagelab.org>
2018-08-01 13:57:32 -0500

    added lab questionnaire

--- qualtrics-data-formatting.R
+++ qualtrics-data-formatting.R
@@ -2,3 +2,7 @@ install.packages("tidyverse")
 library(tidyverse)
 secondary_data_raw <- read_csv("qualtricsraw/Secondary-data-analysis-numeric.csv", col_names = TRUE, skip = 2)
 lab_debrief_raw <- read_csv("qualtricsraw/Lab-debrief-numeric.csv", col_names = TRUE, skip = 2)
+lab_questionnaire <- read_csv("qualtricsraw/Lab-Questionnaire.csv")
+
+#clean lab_questionnaire
+
