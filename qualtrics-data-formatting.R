8785d01eac9f59ad863ecdc666a44818fd76d233
Melanie Soderstrom <melsod@babylanguagelab.org>
2018-08-01 13:07:23 -0500

    update qualtrics files

--- qualtrics-data-formatting.R
+++ qualtrics-data-formatting.R
@@ -1,3 +1,4 @@
 install.packages("tidyverse")
 library(tidyverse)
 secondary_data_raw <- read_csv("qualtricsraw/Secondary-data-analysis-numeric.csv", col_names = TRUE, skip = 2)
+lab_debrief_raw <- read_csv("qualtricsraw/Lab-debrief-numeric.csv", col_names = TRUE, skip = 2)
