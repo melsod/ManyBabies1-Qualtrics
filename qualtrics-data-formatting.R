df2620edc88c415efdaa1a1c961c6d33fd998fa6
mekline <melissa.e.kline@gmail.com>
2018-08-31 12:10:17 -0400

    began adding column names

--- qualtrics-data-formatting.R
+++ qualtrics-data-formatting.R
@@ -1,8 +1,7 @@
-install.packages("tidyverse")
+#install.packages("tidyverse")
 library(tidyverse)
 
 # Read qualtrics data
-
 secondary_header <- read_csv("qualtricsraw/Secondary-data-analysis-numeric.csv", col_names = TRUE, n_max = 2)
 secondary_data_raw <- read_csv("qualtricsraw/Secondary-data-analysis-numeric.csv", col_names = FALSE, skip = 3)
 names(secondary_data_raw) <- names(secondary_header)
@@ -15,7 +14,26 @@ lab_questionnaire_header <- read_csv("qualtricsraw/Lab-Questionnaire.csv", col_n
 lab_questionnaire_raw <- read_csv("qualtricsraw/Lab-Questionnaire.csv", col_names = FALSE, skip = 3)
 names(lab_questionnaire_raw) <- names(lab_questionnaire_header)
 
-#clean lab_questionnaire
+#Get dictionaries so we can add meaningful column names
+secondary_header <- secondary_header %>%
+  rownames_to_column %>% 
+  gather(var, value, -rowname) %>% 
+  spread(rowname, value)
+write_csv(secondary_header, 'metadata/secondary_dict_raw.csv')
+
+lab_debrief_header <- lab_debrief_header %>%
+  rownames_to_column %>% 
+  gather(var, value, -rowname) %>% 
+  spread(rowname, value)
+write_csv(lab_debrief_header, 'metadata/lab_debrief_dict_raw.csv')
+
+lab_questionnaire_header <- lab_questionnaire_header %>%
+  rownames_to_column %>% 
+  gather(var, value, -rowname) %>% 
+  spread(rowname, value)
+write_csv(lab_questionnaire_header, 'metadata/lab_questionnaire_dict_raw.csv')
+
+#Read in dicts with better column names and add them
 
 
 
