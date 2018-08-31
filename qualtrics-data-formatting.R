93caa1345f090267f9242cce4be8cf24b618adbe
mekline <melissa.e.kline@gmail.com>
2018-08-31 11:32:58 -0400

    split dictionary from data

--- qualtrics-data-formatting.R
+++ qualtrics-data-formatting.R
@@ -1,8 +1,21 @@
 install.packages("tidyverse")
 library(tidyverse)
-secondary_data_raw <- read_csv("qualtricsraw/Secondary-data-analysis-numeric.csv", col_names = TRUE, skip = 2)
-lab_debrief_raw <- read_csv("qualtricsraw/Lab-debrief-numeric.csv", col_names = TRUE, skip = 2)
-lab_questionnaire <- read_csv("qualtricsraw/Lab-Questionnaire.csv")
+
+# Read qualtrics data
+
+secondary_header <- read_csv("qualtricsraw/Secondary-data-analysis-numeric.csv", col_names = TRUE, n_max = 2)
+secondary_data_raw <- read_csv("qualtricsraw/Secondary-data-analysis-numeric.csv", col_names = FALSE, skip = 3)
+names(secondary_data_raw) <- names(secondary_header)
+
+lab_debrief_header <- read_csv("qualtricsraw/Lab-debrief-numeric.csv", col_names = TRUE, n_max = 2)
+lab_debrief_raw <- read_csv("qualtricsraw/Lab-debrief-numeric.csv", col_names = FALSE, skip = 3)
+names(lab_debrief_raw) <- names(lab_debrief_header)
+
+lab_questionnaire_header <- read_csv("qualtricsraw/Lab-Questionnaire.csv", col_names = TRUE, n_max = 2)
+lab_questionnaire_raw <- read_csv("qualtricsraw/Lab-Questionnaire.csv", col_names = FALSE, skip = 3)
+names(lab_questionnaire_raw) <- names(lab_questionnaire_header)
 
 #clean lab_questionnaire
+
+
 
