ea2b587b3c8c592a4bb7f171b932c7fb692d2474
mekline <melissa.e.kline@gmail.com>
2018-09-28 12:23:15 -0400

    reconciled about half of main questionnaire for duplicate lines. Some of these are protocol changes! (this is actually a pretty reasonable action by labs, just not what we intended

--- qualtrics-data-formatting.R
+++ qualtrics-data-formatting.R
@@ -125,16 +125,17 @@ lab_questionnaire_raw$lab[lab_questionnaire_raw$lab == "babylablscp"] <- "lscpps
 #Issue #8 Labs still un-matched in questionnaire - did they participate in MB1 or drop out after initial questionnaire?
 setdiff(lab_questionnaire_raw$lab, final_labids$V1)
 
-#lab_questionnaire_raw$lab[lab_questionnaire_raw$lab == "elpgeorgetown"] <-
-#lab_questionnaire_raw$lab[lab_questionnaire_raw$lab == "ellskidmore"] <- 
-#lab_questionnaire_raw$lab[lab_questionnaire_raw$lab == "nusbabylab"] <- "infantlabsingapore"
-#lab_questionnaire_raw$lab[lab_questionnaire_raw$lab == "mqcll"] <- 
-#lab_questionnaire_raw$lab[lab_questionnaire_raw$lab == "infantlabsingapore"] <- 
-
 # AS A TEMPORARY MEASURE, Drop these non-matching IDs from the sheet:
-
 lab_questionnaire_raw <- lab_questionnaire_raw %>%
-  filter(!(lab %in% c("elpgeorgetown", "ellskidmore", "nusbabylab", "mqcll", "infantlabsingapore")))
+  filter(!(lab %in% c("ellskidmore", "nusbabylab","infantlabsingapore")))
+
+# Resolution
+
+#mqcll formally withdrew (staffing/database limitations prevented recruitment)
+#elpgeorgetown only doing gaze following
+lab_questionnaire_raw <- lab_questionnaire_raw %>%
+  filter(!(lab %in% c("mqcll", "elpgeorgetown")))
+
 
 
 ####-----> Issue #9 - (final dataset) labs that didn't fill out one of the surveys. Is this okay?
@@ -151,5 +152,86 @@ setdiff(no_debrief, no_secondary)
 intersect(no_debrief, no_secondary)
 
 
+#########
+####
+# CONSOLIDATE RESPONSES: Some labs have more than one entry in a spreadsheet! 
+# Sometimes this is just an initial blank form, but it could also be a mistake/inconsistency we need to
+# check. Manually examine these before merging. 
+####
+#########
 
+lab_questionnaire_raw <- lab_questionnaire_raw %>%
+  arrange(lab)
 
+q_duplicates <- lab_questionnaire_raw %>%
+  group_by(lab)%>%
+  summarize(n=n())%>%
+  filter(n > 1)
+
+View(filter(lab_questionnaire_raw, lab %in% q_duplicates$lab))
+foo  = filter(lab_questionnaire_raw, lab == 'babylabutrecht')
+
+# 1 babylableiden           2
+# Most info is in second fill-out only, completed 21-03-18. Earlier version (08-02-2018) states collection to an 
+#N, starting 25-01-18, updated version states collection to last day (04-30-2018). No PCF entry. 
+#RESOLUTION: Copy start date to 2nd version, drop first. 
+#TODO: Contact about protocol change?
+
+lab_questionnaire_raw <- lab_questionnaire_raw %>%
+  mutate(PlannedStart = ifelse(lab == 'babylableiden', 'January 25, 2018', PlannedStart))%>%
+  filter(!(lab == 'babylableiden' & StartDate == '2018-02-01 04:48:42'))
+
+# 2 babylablmu              2
+# Minor updates to text answers
+# RESOLUTION: Drop earlier version, add a couple of expanded answers from first version
+
+lab_questionnaire_raw <- lab_questionnaire_raw %>%
+  filter(!(lab == 'babylablmu' & StartDate == '2017-06-20 04:26:07')) %>%
+  mutate(Recruitment = ifelse(lab == 'babylablmu', "The city of Munich provides us with a database of all families. We send invitation letters to families that have children in the age range that we are interested. Approximately 10% or less reply to our letters, so if we want to test for instance 40 children we usually send about 400 letters. We also have a separate database of \"returning\" families, that expressed interest for being contacted for more studies. We call these families directly on the phone. \nWe generally consider that the 5% will contact us back for an appointment. So, for instance, for 20 children we send letters to 400 families."                                                                                                                                       , Recruitment))%>%
+  mutate(Compensation_toysBooks = ifelse(lab == 'babylablmu',"5 € value, children can choose one toy/book",Compensation_toysBooks))%>%
+  mutate(RA_labTraining = ifelse(lab == 'babylablmu', "one reseach assistant (Alyssa Torske) is a graduate student, is involved in the study and has read all the manybabies material online. She helped setting up the study. Another RA will help with recruiting and data collection but is not intellectually involved in this study. She will try the experimental procedure with me until she is familiar with it. She has already experience testing babies for other studies.\nBoth RAs worked with the eye-tracker previously, so they knew the software. First the RA try the procedure with one another (one plays the participant) under my supervision. Then, each RA tests 3 pilot children."  ,RA_labTraining))
+
+# 3 babylabnijmegen         2
+# Second is minor expansion of first; no inconsistencies.
+# RESOLUTION: Drop earlier, add a couple of expanded answers from first version
+
+lab_questionnaire_raw <- lab_questionnaire_raw %>%
+  filter(!(lab == 'babylabnijmegen' & StartDate == '2017-09-13 03:27:55')) %>%
+  mutate(Compensation_toysBooks = ifelse(lab == 'babylabnijmegen', "1 book worth ca. 10€", Compensation_toysBooks))%>%
+  mutate(RA_labTraining = ifelse(lab == 'babylabnijmegen', "the RA was recently trained for another headturn study of the main contributor from this lab. She first observed 3 test sessions run by me, Laura Hahn. Then she ran two mock sessions, with a teddy bear instead of a real baby. The online coding was trained using videos of previous sessions and pressing the respective buttongs on the keyboard (as if she was coding the video), wearing headphones with the same distractor music as in the test session. Then she tested her first baby with me observing the test session. At all stages, she received feedback about her performance.\nRA reads lab instruction doc carefully RA observes studies in the lab RA runs three experimental sessions together with a more senior lab user RA gets feedback from senior lab user throughout the previous step RA practices look coding with videos of previous experimental sessions RA tests independently"       , RA_labTraining))%>%
+  mutate(RA_labTraining_criteria = ifelse(lab == 'babylabnijmegen', "1 book worth ca. 10€", RA_labTraining_criteria))%>%
+  mutate(BlinkRate_lights_other = ifelse(lab=='babylabnijmegen',"rate of 250ms, meaning the lamp turns off after 250 ms then off after another 250ms then on again and so on, sums up to 4 per second",BlinkRate_lights_other))%>%
+  mutate(Exprimenter_HeadphoneType = ifelse(lab == 'babylabnijmegen', "room is dimly lit, experimenter stands behind curtain, observing the infant via camera, experimenter wears cushion headphones and is blind for the condition of the trial but not blind for trial number", Exprimenter_HeadphoneType))
+  
+# 4 babylabumassb           2
+# Second fill-out just has more questions answered; drop first!
+
+lab_questionnaire_raw <- lab_questionnaire_raw %>%
+  filter(!(lab == 'babylabumassb' & StartDate == '2017-07-05 13:17:17'))
+
+# 5 babylabutrecht          2
+# The second one just reports dBA, which was missing from first
+
+lab_questionnaire_raw <- lab_questionnaire_raw %>%
+  filter(!(lab == 'babylabutrecht' & StartDate == '2018-01-09 04:06:57')) %>%
+  mutate(dBA == ifelse(lab == 'babylabutrecht',"The maximum reached power during playback of the reference audio was 75.0±0 dB(A) SPL, 35.6±1.6 without (n=3, 10 sec.).",dBA))
+
+# 6 babylabyork             2
+# Second form primarily just adding more information, BUT the subject planned contribution bins also change. 
+#TODO: Explore more!
+#older version: 2017-05-05 08:39:16
+#PlannedStart: We saw the first participant on May 4. (Amended to 'no')
+
+# 7 baldwinlabuoregon       3
+# 8 cfnuofn                 2
+# 9 chosunbaby              2
+# 10 escompicbsleipzig       2
+# 11 infantstudiesubc        2
+# 12 kokuhamburg             3
+# 13 kyotobabylab            2
+# 14 lancaster               2
+# 15 langlabucla             2
+# 16 lscppsl                 2
+# 17 trainorlab              4
+# 18 unlvmusiclab            2
+# 19 weescienceedinburgh     2
